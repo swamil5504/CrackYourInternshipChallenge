5c70bbafd4ce3d5610a49586237eb7895f7cdf66
Swamil Randive <swamilrandive47@gmail.com>
2024-08-06 18:04:42 +0530

    6/8/24

--- linkListCycle.cpp
+++ linkListCycle.cpp
@@ -1,28 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <unordered_map>
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *slow = head;
-        ListNode *fast = head;
+        unordered_map<ListNode*, int> mpp;
+        ListNode* temp = head; 
 
-        while(fast != NULL && fast->next != NULL)
-        {
-            slow = slow -> next;
-            fast = fast -> next ->next;
-
-            if(slow == fast)
-            {
+        while (temp != NULL) {
+            if (mpp.find(temp) != mpp.end()) {
                 return true;
             }
+
+            mpp[temp] = 1;
+            
+            temp = temp->next;
         }
-         return false;
+        return false;
     }
-   
 };
